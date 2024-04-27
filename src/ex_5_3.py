f5374703098e50ee00d399506e5cb8b8673bd86d
ManiSaiYelleni <164649487+ManiSaiYelleni@users.noreply.github.com>
2024-04-27 10:53:09 -0500

    Update ex_5_3.py
    Completed 5_3

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -7,6 +7,10 @@ This module contains an entry point that:
 """
 import numpy as np
 from argparse import ArgumentParser
+try:
+    from src.util import get_repository_root
+except ImportError:
+    from util import get_repository_root
 
 if __name__ == "__main__":
     # Create your argument parser object here.
@@ -17,11 +21,9 @@ if __name__ == "__main__":
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
     import os
-
-    parser = ArgumentParser(
-        description='This programme writes the data from the input file to the output file after applying a standard scale transform..')
-    parser.add_argument('infile', help='provide input file path', nargs='?')
-    parser.add_argument('outfile', help='provide output file path', nargs='?')
+    parser = ArgumentParser(description='This programme writes the data from the input file to the output file after applying a standard scale transform..')
+    parser.add_argument('infile',help='provide input file path',nargs='?')
+    parser.add_argument('outfile',help='provide output file path',nargs='?')
     args = parser.parse_args()
     input_data = np.loadtxt(args.infile)
     normalized = (input_data - input_data.mean(axis=0)) / input_data.std(axis=0)
@@ -29,4 +31,5 @@ if __name__ == "__main__":
     root_dir = get_repository_root()
     os.makedirs(root_dir / "outputs", exist_ok=True)
     np.savetxt(args.outfile, processed, fmt='%.2e')
+
 
